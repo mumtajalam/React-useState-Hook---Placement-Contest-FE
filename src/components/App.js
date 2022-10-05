63934d38d9a1b227b34c300b96d935c9c0e24a14
md_mumtaj_alam <mmumtajalam987@gmail.com>
2022-10-05 12:11:39 +0530

    up

--- src/components/App.js
+++ src/components/App.js
@@ -4,28 +4,23 @@ import React, { useState } from "react";
 export default function App() {
   //code here
   const [inputValue, setInput] = useState();
-  const [text, setText] = useState([]);
+  const [text, setText] = useState("");
 
   const changeInput = (e) => {
     setInput(e.target.value);
   };
 
-  const buttonClick = () => {
-    setText([...text, inputValue]);
+  const buttonClick = (e) => {
+    e.preventDefault();
+    setText(text + inputValue);
     setInput("");
   };
 
   return (
     <div>
       <input id="input" value={inputValue} onChange={changeInput} />
-      <p id="text">String Concate</p>
-      {text &&
-        text.map((item, index) => (
-          <p key={item + index} id="intro">
-            {item}
-          </p>
-        ))}
-
+      <p id="intro">Concated String</p>
+      <p id="text">{text}</p>
       <button id="button" onClick={buttonClick}>
         Click me
       </button>
